7374ed4afe1dfbd9fe04b25c506c82784f439ba0
JimmyYang20 <yangjin39@huawei.com>
2021-09-15 10:32:26 +0800

    dd
    Signed-off-by: JimmyYang20 <yangjin39@huawei.com>

--- joint-inference-example/init-background.sh
+++ joint-inference-example/init-background.sh
@@ -41,4 +41,3 @@ kubectl taint node $HOSTNAME node-role.kubernetes.io/master-
 # Install Sedna control components in one command, you can run:
 curl https://raw.githubusercontent.com/kubeedge/sedna/main/scripts/installation/install.sh | SEDNA_GM_NODE=$HOSTNAME SEDNA_ACTION=create bash -
 
-sleep 20
--- joint-inference-example/init-foreground.sh
+++ joint-inference-example/init-foreground.sh
@@ -4,12 +4,13 @@ FREQUENCY=1                                          # Delay between each check 
 BACKGROUND_SIGNAL_FILE='/opt/.backgroundfinished'    # File updated by background to indicate completion
 BACKGROUND_SAFE_WORD='done'                          # Word in BACKGROUND_SIGNAL_FILE indicating completion
 START_MESSAGE='Starting scenario'                    # Message before the progress animation
-END_NORMAL_MESSAGE='Scenario ready. You have a running Kubernetes cluster.'
+END_NORMAL_MESSAGE='Scenario ready. You have a running Sedna.'
 END_KILLED_MESSAGE='Interupted. This scenario may still be initializing.'
 
 SPINNER_COLOR_NUM=2                # Color to use, unless COLOR_CYCLE=1
 SPINNER_COLOR_CYCLE=0              # 1 to rotate colors between each animation
 INTERVAL=.25
+WAITTING_SEDNA_DEPLOYMENT=20
 SPINNER_NORMAL=$(tput sgr0)        # Reset encoding
 symbols=("▐⠂       ▌" "▐⠈       ▌" "▐ ⠂      ▌" "▐ ⠠      ▌" "▐  ⡀     ▌" "▐  ⠠     ▌" "▐   ⠂    ▌" "▐   ⠈    ▌" "▐    ⠂   ▌" "▐    ⠠   ▌" "▐     ⡀  ▌" "▐     ⠠  ▌" "▐      ⠂ ▌" "▐      ⠈ ▌" "▐       ⠂▌" "▐       ⠠▌" "▐       ⡀▌" "▐      ⠠ ▌" "▐      ⠂ ▌" "▐     ⠈  ▌" "▐     ⠂  ▌" "▐    ⠠   ▌" "▐    ⡀   ▌" "▐   ⠠    ▌" "▐   ⠂    ▌" "▐  ⠈     ▌" "▐  ⠂     ▌" "▐ ⠠      ▌" "▐ ⡀      ▌" "▐⠠       ▌")
 
@@ -66,6 +67,8 @@ start_progress () {
   done
 
   stty sane; tput cnorm; clear
+
+  sleep ${WAITTING_SEDNA_DEPLOYMENT}
   printf "%s\n\n" "${end_message}"
   
   # Pick up any changes during background
