c5c295e4664eaf09136f4fc4c94022588c62a254
JimmyYang20 <yangjin39@huawei.com>
2021-09-15 10:56:25 +0800

    dd
    Signed-off-by: JimmyYang20 <yangjin39@huawei.com>

--- joint-inference-example/init-background.sh
+++ joint-inference-example/init-background.sh
@@ -38,6 +38,5 @@ kubectl taint node $HOSTNAME node-role.kubernetes.io/master-
 
 # Install Sedna control components in one command, you can run:
 curl https://raw.githubusercontent.com/kubeedge/sedna/main/scripts/installation/install.sh | SEDNA_GM_NODE=$HOSTNAME SEDNA_ACTION=create bash -
-sleep 20
 
 echo "done" >> /opt/.backgroundfinished
